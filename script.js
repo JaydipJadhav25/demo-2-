c4133e65d0cebb8f516a24c0b64cfcd3a1b11bc5
jaydipjadhav <jaydipdjadhav>
2024-01-20 17:21:48 +0530

    add 2 files

---
+++ script.js
@@ -0,0 +1,17 @@
+let a =10;
+let b= 20;
+
+console.log("a =",a);
+console.log("b =",b);
+console.log(a+b);
+console.log(a+b);
+try{
+console.log(a-c);
+
+}
+catch(err){
+    console.log("error");
+    console.log(err);
+}
+console.log(a*b);
+console.log(a/b);
